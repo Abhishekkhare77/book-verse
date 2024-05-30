7014cb22613794037c4938656c01862220c3abd2
Abhishek khare <ghostabhi323@gmail.com>
2024-05-30 07:23:50 +0530

    Update layout and add Navbar component

---
+++ src/config/index.ts
@@ -0,0 +1,46 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui-kits" as const,
+        featured: [
+            {
+                name: "Editors Pick",
+                href: "#",
+                imgSrc: "nav/mixed.jpg"
+            },
+            {
+                name: "Trending",
+                href: "#",
+                imgSrc: "nav/mixed.jpg"
+            },
+            {
+                name: "Newest",
+                href: "#",
+                imgSrc: "nav/mixed.jpg"
+            }
+        ]
+
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imgSrc: "nav/mixed.jpg"
+            },
+            {
+                name: "Trending",
+                href: "#",
+                imgSrc: "nav/mixed.jpg"
+            },
+            {
+                name: "Newest",
+                href: "#",
+                imgSrc: "nav/mixed.jpg"
+            }
+        ]
+
+    }
+]
