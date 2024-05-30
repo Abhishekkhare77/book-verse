3120ea61309d3a2e1df47b071097ed7461959e28
Abhishek khare <ghostabhi323@gmail.com>
2024-05-30 08:13:05 +0530

    Add Image and Link imports to NavItem component

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,17 +6,17 @@ export const PRODUCT_CATEGORIES = [
             {
                 name: "ReactJs",
                 href: "#",
-                imgSrc: "nav/mixed.jpg"
+                imgSrc: "/nav/mixed.jpg"
             },
             {
                 name: "VueJs",
                 href: "#",
-                imgSrc: "nav/mixed.jpg"
+                imgSrc: "/nav/mixed.jpg"
             },
             {
                 name: "AngularJs",
                 href: "#",
-                imgSrc: "nav/mixed.jpg"
+                imgSrc: "/nav/mixed.jpg"
             }
         ]
 
@@ -28,17 +28,17 @@ export const PRODUCT_CATEGORIES = [
             {
                 name: "NodeJs",
                 href: "#",
-                imgSrc: "nav/mixed.jpg"
+                imgSrc: "/nav/mixed.jpg"
             },
             {
                 name: "Django",
                 href: "#",
-                imgSrc: "nav/mixed.jpg"
+                imgSrc: "/nav/mixed.jpg"
             },
             {
                 name: "Laravel",
                 href: "#",
-                imgSrc: "nav/mixed.jpg"
+                imgSrc: "/nav/mixed.jpg"
             }
         ]
 
